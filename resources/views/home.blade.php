c35152bffc1e241caf0c79d602a9ef837216b2d2
Cale Bybee <bybeecale@gmail.com>
2024-05-10 20:27:48 -0600

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta
+            name="description"
+            content="Cale Bybee's Portfolio"
+        />
+        <title>Cale Bybee</title>
+        <link rel="icon" href="{{ asset('images/favicon-32x32.png') }}">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+        />
+
+        <!-- Styles -->
+        <style>
+        </style>
+    </head>
+</html>
